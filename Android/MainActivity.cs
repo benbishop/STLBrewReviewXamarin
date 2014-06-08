36e79f7f8f365f68b29fafd1c690b01ea761a2de
Ben Bishop <ben@rendr.io>
2014-06-08 08:47:18 -0500

    Made Android only support portrait mode.

--- Android/MainActivity.cs
+++ Android/MainActivity.cs
@@ -13,7 +13,7 @@ using STLBrewReview.Mobile;
 
 namespace STLBrewReviewMobile.Android
 {
-	[Activity (Label = "STLBrewReviewMobile.Android.Android", MainLauncher = true)]
+	[Activity (Label = "STLBrewReviewMobile.Android.Android", MainLauncher = true, ScreenOrientation = global::Android.Content.PM.ScreenOrientation.Portrait)]
 	public class MainActivity : AndroidActivity
 	{
 		protected override void OnCreate (Bundle bundle)
