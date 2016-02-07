37d4509d54246614ef9a89d3bfd7828b390bcf12
Ben Bishop <ben@rendr.io>
2016-02-07 14:40:45 -0600

    Fixed Android Compile errors.

--- Android/Views/BreweryDetailsViewRenderer.cs
+++ Android/Views/BreweryDetailsViewRenderer.cs
@@ -8,44 +8,47 @@ using Android.Content;
 using Android.Content.PM;
 
 
-[assembly:ExportRenderer (typeof(BreweryDetailsView), typeof(BreweryDetailsViewRenderer))]
+[assembly:ExportRenderer(typeof(BreweryDetailsView), typeof(BreweryDetailsViewRenderer))]
 namespace STLBrewReviewMobile.Android
 {
-	public class BreweryDetailsViewRenderer:PageRenderer
-	{
-		public BreweryDetailsViewRenderer ()
-		{
-		}
+    public class BreweryDetailsViewRenderer:PageRenderer
+    {
+        public BreweryDetailsViewRenderer()
+        {
+        }
 
-		protected override void OnModelChanged (VisualElement oldModel, VisualElement newModel)
-		{
-			BreweryDetailsView view = (BreweryDetailsView)newModel;
-			view.MakePhoneCall += CreatePhoneCallIntent;
-			view.OpenEmail += CreateEmailIntent;
-			view.LaunchMapApp += LaunchMapApp;
-			base.OnModelChanged (oldModel, newModel);
-		}
+        protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+        {
+            BreweryDetailsView view = (BreweryDetailsView)e.NewElement;
+            view.MakePhoneCall += CreatePhoneCallIntent;
+            view.OpenEmail += CreateEmailIntent;
+            view.LaunchMapApp += LaunchMapApp;
 
-		void CreatePhoneCallIntent (string phoneNumber)
-		{
-			var uri = global::Android.Net.Uri.Parse (phoneNumber);
-			var intent = new Intent (Intent.ActionView, uri); 
-			this.Context.StartActivity (intent);
-		}
+            base.OnElementChanged(e);
+        }
 
-		void CreateEmailIntent (string email)
-		{
-			var intent = new Intent (Intent.ActionSend);
-			intent.PutExtra (Intent.ExtraEmail, new string[]{ email });
-			intent.SetType ("message/rfc822");
-			this.Context.StartActivity (intent);
-		}
 
-		void LaunchMapApp (string address)
-		{
-			Intent intent = new Intent (Intent.ActionView, global::Android.Net.Uri.Parse ("google.navigation:q=" + address));
-			this.Context.StartActivity (intent);
-		}
-	}
+
+        void CreatePhoneCallIntent(string phoneNumber)
+        {
+            var uri = global::Android.Net.Uri.Parse(phoneNumber);
+            var intent = new Intent(Intent.ActionView, uri); 
+            this.Context.StartActivity(intent);
+        }
+
+        void CreateEmailIntent(string email)
+        {
+            var intent = new Intent(Intent.ActionSend);
+            intent.PutExtra(Intent.ExtraEmail, new string[]{ email });
+            intent.SetType("message/rfc822");
+            this.Context.StartActivity(intent);
+        }
+
+        void LaunchMapApp(string address)
+        {
+            Intent intent = new Intent(Intent.ActionView, global::Android.Net.Uri.Parse("google.navigation:q=" + address));
+            this.Context.StartActivity(intent);
+        }
+    }
 }
 
